11e12a4d5e0796ed349bb8bb4f4bda690c8a8d0a
Dennis Huaman <dennishy197@gmail.com>
2025-10-09 10:30:35 +0200

    actualización user_id, role
    Se cambió la clase Attendee por User, genera el user_id y el rol de cada individuo, asimismo, se añadió el EVENT_TYPE "FIRE"

--- Pruebas/attendees_aws_sqs.py
+++ Pruebas/attendees_aws_sqs.py
@@ -19,8 +19,9 @@ LOCATIONS = [
 
 # --- Tipos de evento---
 EVENT_TYPES = {
-    "movimiento": 0.4,       # Probabilidad de que el asistente se mueve
-    "alerta_seguridad": 0.01  # Probabilidad de generar una alerta
+    "MOVEMENT": 0.4,       # Probabilidad de que el asistente se mueve
+    "SUSPICIOUS ACTIVITY": 0.05,  # Probabilidad de generar una alerta
+    "FIRE": 0.01  # Probabilidad de generar una alerta
 }
 
 # Constantes para los niveles de observación
@@ -28,17 +29,18 @@ CROWD_LEVELS = ["bajo", "moderado", "alto", "abarrotado"]
 QUEUE_LEVELS = ["corta", "media", "larga", "muy larga"]
 
 
-class Attendee:
+class User:
     """
-    Representa a un asistente del festival. En cada acción, reporta un
+    Representa a un usuario general del festival. En cada acción, reporta un
     estado completo que incluye signos vitales, y observaciones del entorno.
     """
 
     def __init__(self):
-        self.attendee_id = str(uuid.uuid4())
+        self.user_id = str(uuid.uuid4())
+        self.role = "Attendee"
         self.location = "Ingreso Principal"
         self.heart_rate = random.randint(70, 90)
-        print(f"Asistente {self.attendee_id} creado en {self.location}")
+        print(f"Asistente {self.user_id} creado en {self.location}")
 
     def _update_vitals_and_observations(self):
         """
@@ -83,7 +85,8 @@ class Attendee:
 
         # Prepara la información base del evento
         event_payload = {
-            "attendee_id": self.attendee_id,
+            "user_id": self.user_id,
+            "role": self.role,
             "timestamp": datetime.utcnow().isoformat() + "Z",
             "location": self.location,
             "event_type": chosen_event
@@ -91,15 +94,20 @@ class Attendee:
 
         # Realiza la acción principal
         action_data = {}
-        if chosen_event == "movimiento":
+        if chosen_event == "MOVEMENT":
             new_location = random.choice(
                 [loc for loc in LOCATIONS if loc != self.location or "Ingreso Principal"])
             self.location = new_location
             action_data = {"destino": new_location}
 
-        elif chosen_event == "alerta_seguridad":
+        elif chosen_event == "SUSPICIOUS ACTIVITY":
             self.heart_rate = random.randint(130, 160)  # Simula estrés
             action_data = {"descripcion": "Actividad sospechosa detectada."}
+
+        elif chosen_event == "FIRE":
+            self.heart_rate = random.randint(130, 160)  # Simula estrés
+            action_data = {
+                "descripcion": "Fuego detectado en {self.location}."}
 
         current_state_data = self._update_vitals_and_observations()
 
@@ -111,19 +119,17 @@ class Attendee:
 
 # --- Bucle Principal de la Simulación ---
 if __name__ == "__main__":
-    NUM_ATTENDEES = 1
-    attendees = [Attendee() for _ in range(NUM_ATTENDEES)]
+    NUM_USERS = 2
+    users = [User() for _ in range(NUM_USERS)]
 
     print("\n--- Iniciando Simulación con Datos de Estado Completos ---\n")
 
     try:
         queue = sqs.get_queue_by_name(QueueName='BackStageOps-queue')
-        # print(queue.url)
 
         while True:
-            random_attendee = random.choice(attendees)
+            random_attendee = random.choice(users)
             json_event = random_attendee.simulate_action()
-            # print(type(json_event))
 
             if json_event:
                 print(json_event)
--- Pruebas/lambda_funcion.py
+++ Pruebas/lambda_funcion.py
@@ -4,25 +4,9 @@ import json
 def lambda_handler(message, context):
     try:
         message = json.loads(message)
-        attendee_id = message.get('attendee_id')
-        print(f'Cliente {attendee_id} conectado.!')
+        user_id = message.get('attendee_id')
+        print(f'Cliente {user_id} conectado.!')
         # Let the queue know that the message is processed
         # message.delete()
     except:
         print("No hay datos para procesar")
-
-# import json
-
-
-# def lambda_handler(event, context):
-#     # Imprime el evento recibido para depuración
-#     print(f"Evento recibido: {json.dumps(event)}")
-
-#     # Extrae un nombre del evento o usa 'mundo' por defecto
-#     nombre = event.get('nombre', 'mundo')
-
-#     # Devuelve un saludo
-#     return {
-#         'statusCode': 200,
-#         'body': json.dumps(f'¡Hola, {nombre} desde Lambda!')
-#     }
